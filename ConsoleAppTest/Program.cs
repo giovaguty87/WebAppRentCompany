303f81fd4374161e33e98686a10f362438f10fee
CARVAJAL\giogutgi <giovanny.gutierrez@carvajal.com>
2019-04-03 18:07:31 -0500

    Add projects unit test and consoleApp

---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest
+{
+    public class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void AddDriver()
+        {
+            BGlobalEntities ctx = new BGlobalEntities();
+
+            Driver driver = new Driver();
+
+            driver.Id_driver = 1;
+            driver.Name = "Juancho";
+            driver.DNI = "10548875";
+            driver.Address = "Street";
+            driver.Telephone = "31134000";
+            driver.Number_card = "54712900";
+
+            ctx.Drivers.Add(driver);
+
+            ctx.SaveChanges();
+        }
+
+        static void Main(string[] args)
+        {
+            AddDriver();
+        }
+    }
+}
---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ac34f2e7-4f1e-4f96-bcc2-3f277d965159")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

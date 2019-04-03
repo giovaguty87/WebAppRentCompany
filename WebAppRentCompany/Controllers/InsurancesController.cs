81b49a5d3224f34cc5216d3b739f197f7ef1086d
giovaguty87 <gutygio87@gmail.com>
2019-04-03 08:23:13 -0500

    WebAppRentCompany

---
+++ WebAppRentCompany/Controllers/InsurancesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebAppRentCompany.Models;
+
+namespace WebAppRentCompany.Controllers
+{
+    public class InsurancesController : Controller
+    {
+        private BGlobalEntities10 db = new BGlobalEntities10();
+
+        // GET: Insurances
+        public ActionResult Index()
+        {
+            return View(db.Insurances.ToList());
+        }
+
+        // GET: Insurances/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insurance insurance = db.Insurances.Find(id);
+            if (insurance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insurance);
+        }
+
+        // GET: Insurances/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Insurances/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id_insurance,Description")] Insurance insurance)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Insurances.Add(insurance);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(insurance);
+        }
+
+        // GET: Insurances/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insurance insurance = db.Insurances.Find(id);
+            if (insurance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insurance);
+        }
+
+        // POST: Insurances/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id_insurance,Description")] Insurance insurance)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(insurance).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(insurance);
+        }
+
+        // GET: Insurances/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insurance insurance = db.Insurances.Find(id);
+            if (insurance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insurance);
+        }
+
+        // POST: Insurances/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Insurance insurance = db.Insurances.Find(id);
+            db.Insurances.Remove(insurance);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
